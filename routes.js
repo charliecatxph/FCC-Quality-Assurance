c68973c3015076212ead6822ab35838bef4b539b
Charl Concepcion <concepcioncharl2@gmail.com>
2022-02-13 13:42:06 +0800

    Upload from Glitch

--- routes.js
+++ routes.js
@@ -17,6 +17,7 @@ module.exports = function (app, myDataBase) {
       message: "Please login",
       showLogin: true,
       showRegistration: true,
+      showSocialAuth: true
     });
   });
 
@@ -73,6 +74,12 @@ module.exports = function (app, myDataBase) {
       res.redirect("/profile");
     }
   );
+  
+  app.route('/auth/github').get(passport.authenticate('github'));
+  
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile', { username : req.displayName });
+  });
 
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
